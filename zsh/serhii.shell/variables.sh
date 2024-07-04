ce4f9ef372688235e86ab546fafaa77427d742eb
serhii.dudar <serhii.dudar@globallogic.com>
2024-07-04 15:51:01 +0300

    fzf, zsh config change, bat, fd, ripgrep, tree...

--- zsh/serhii.shell/variables.sh
+++ zsh/serhii.shell/variables.sh
@@ -6,9 +6,24 @@ if isMacOs; then
     # Add Docker Desktop for Mac (docker)
     export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin/"
 else
-    export PATH=$PATH:/home/serhii/homebrew/bin/
-    export PATH=$PATH:/snap/bin
+    #export PATH=$PATH:/home/serhii/homebrew/bin/
+    #export PATH=$PATH:/snap/bin
     export PATH=$PATH:/home/serhii/serhii.home/tools/kafka/bin
+
+    # in case installed by brew
+    # sudo ln -s /home/serhii/homebrew/bin//fd /usr/local/bin/fd
+    # sudo ln -s /home/serhii/homebrew/bin//bat /usr/local/bin/bat
+    # sudo ln -s /home/serhii/homebrew/bin//fastfetch /usr/local/bin/fastfetch
+    # sudo ln -s /home/serhii/homebrew/bin//rg /usr/local/bin/rg
+    # sudo ln -s /home/serhii/homebrew/bin//nvim /usr/local/bin/nvim
+    # sudo ln -s /home/serhii/homebrew/bin//eza /usr/local/bin/eza
+    # sudo ln -s /home/serhii/homebrew/bin//k9s /usr/local/bin/k9s
+    # sudo ln -s /home/serhii/homebrew/bin//tldr /usr/local/bin/tldr
+    # sudo ln -s /home/serhii/homebrew/bin//fzf /usr/local/bin/fzf
+    # sudo ln -s /home/serhii/homebrew/bin//zellij /usr/local/bin/zellij
+    # sudo ln -s /home/serhii/homebrew/bin//procs /usr/local/bin/procs
+
+    # to rofi: sudo ln -s /home/serhii/homebrew/bin//fd /usr/bin/fd
 fi
 
 export VISUAL='nvim'
