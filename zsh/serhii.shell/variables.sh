39976aff125d125462223a9c46f6aafa82e6bbf2
serhii.dudar <serhii.dudar@globallogic.com>
2024-08-02 12:11:50 +0300

    zsh,yazi

--- zsh/serhii.shell/variables.sh
+++ zsh/serhii.shell/variables.sh
@@ -11,9 +11,6 @@ else
     export PATH=$PATH:/home/serhii/serhii.home/tools/kafka/bin
     export PATH=$PATH:/home/serhii/tools/temporal
 
-    # in case installed by brew
-    # sudo ln -s /home/serhii/homebrew/bin//brew /usr/local/bin/brew
-
     # sudo ln -s /home/serhii/homebrew/bin//fd /usr/local/bin/fd
     # sudo ln -s /home/serhii/homebrew/bin//bat /usr/local/bin/bat
     # sudo ln -s /home/serhii/homebrew/bin//fastfetch /usr/local/bin/fastfetch
