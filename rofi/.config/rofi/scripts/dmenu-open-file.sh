8cdad0e1f04aa0b15bef7740d988699594d9cb9c
serhii.dudar <serhii.dudar@globallogic.com>
2024-08-16 16:48:31 +0300

    rofi

--- rofi/.config/rofi/scripts/dmenu-open-file.sh
+++ rofi/.config/rofi/scripts/dmenu-open-file.sh
@@ -22,7 +22,7 @@ if [[ -z "$1" ]]; then
         /home/serhii/serhii.shell | \
     xargs -I {} printf "nvim {}\nsubl {}\n" \
     ; \
-    /usr/bin/fd . --max-depth 2 --type f --hidden --no-follow \
+    /usr/bin/fd . --max-depth 2 --type f --no-follow \
         /home/serhii/.config/ranger \
         /home/serhii/.config/rofi \
         /home/serhii/.config/qtile \
@@ -32,11 +32,11 @@ if [[ -z "$1" ]]; then
         /home/serhii/.config/alacritty | \
     xargs -I {} printf "nvim {}\nsubl {}\n" \
     ; \
-    /usr/bin/fd . --type f --hidden --no-follow \
+    /usr/bin/fd . --type f --no-follow \
         /home/serhii/dotfiles | \
     xargs -I {} printf "nvim {}\nsubl {}\n" \
     ; \
-    /usr/bin/fd . --type d --max-depth 1 --hidden \
+    /usr/bin/fd . --type d --max-depth 1 \
      /home/serhii/serhii.home/work/git.work \
 	 /home/serhii/serhii.home/personal/git | \
     xargs -I {} printf "$intellijView {}\n$vscodeView {}\n" \
