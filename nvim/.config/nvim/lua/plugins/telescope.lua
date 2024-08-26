65cfcfb2ea9663efdaa91c2b4df6592717d4fefd
serhii.dudar <serhii.dudar@globallogic.com>
2024-08-27 01:11:51 +0300

    harpoon

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,10 @@ return {
         "nvim-telescope/telescope-ui-select.nvim",
     },
     {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
         dependencies = { "nvim-lua/plenary.nvim" },
@@ -30,7 +34,9 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
+            vim.keymap.set("n", "<leader>fd", ":Telescope file_browser<CR>", {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- vim/tests/nvim.t/.config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("boot.base") -- General Settings
-require("boot.highlights") -- Colourscheme and other highlights
-require("boot.maps") -- Keymaps
-require("boot.plugins") -- Plugins
-require("boot.bootstrap") -- Packer Auto-Installer
