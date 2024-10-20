3a84818f0b3f3cebe71789f2a8c9093abd06505c
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-10-20 19:57:13 +0300

    neovim java env configuration

--- nvim/.config/nvim/lua/plugins/ui/color-schema.lua
+++ nvim/.config/nvim/lua/plugins/ui/color-schema.lua
@@ -1,11 +1,30 @@
 return {
   {
       "catppuccin/nvim",
+      name = 'catppuccin',
+      priority = 1000,
+      optional = false,
+      lazy = false,
+      opts = {
+          custom_highlights = function()
+              return {
+                  Visual = { bg = '#373267' },
+              }
+          end,
+      },
       --[[opts = {
           term_colors = true,
           transparent_background = true
       }]]
   },
+  --[[{
+      'rose-pine/neovim',
+      name = 'rose-pine',
+      priority = 1000,
+      optional = false,
+      lazy = false,
+      opts = { highlight_groups = { Visual = { bg = '#373267' } } },
+  },]]
   --[[{
       "rebelot/kanagawa.nvim",
       priority = 1000
