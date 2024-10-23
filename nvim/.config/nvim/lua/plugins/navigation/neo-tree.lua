a02afc0dd4f39a1c99cd56540f36f3ecb4f4004f
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-10-23 11:37:10 +0300

    changes

--- nvim/.config/nvim/lua/plugins/navigation/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/navigation/neo-tree.lua
@@ -1,47 +1,47 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-        source_selector = {
-            winbar = true,
-            statusline = false
-        },
-        window = {
-            mappings = {
-            --    e = 'none'
-                ["/"] = "noop"
-            },
-            --position = 'float',
-        },
-        popup_border_style = 'rounded', --'solid',
-        filesystem = {
-            filtered_items = {
-                visible = true,
-                hide_dotfiles = false,
-                hide_gitignored = true,
-            },
-            follow_current_file = {
-                enabled = true, -- This will find and focus the file in the active buffer every time
-                --               -- the current file is changed while the tree is open.
-                leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-            },
-            group_empty_dirs = true
-        },
-        event_handlers = {
-            {
-                event = "neo_tree_buffer_enter",
-                handler = function()
-                    vim.opt_local.relativenumber = true
-                end,
-            }
-        },
-        default_component_configs = {
-            last_modified = {
-                enabled = true,
-                required_width = 75
-            },
-            file_size = {
-                enabled = false
-            }
-        }
-    }
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    source_selector = {
+      winbar = true,
+      statusline = false,
+    },
+    window = {
+      mappings = {
+        --    e = 'none'
+        ["/"] = "noop",
+      },
+      --position = 'float',
+    },
+    popup_border_style = "rounded", --'solid',
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      group_empty_dirs = true,
+    },
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
+      },
+    },
+    default_component_configs = {
+      last_modified = {
+        enabled = true,
+        required_width = 75,
+      },
+      file_size = {
+        enabled = false,
+      },
+    },
+  },
 }
