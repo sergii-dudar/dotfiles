5674e10e16ab936e707bb5666b65acb194a8477a
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-10-30 17:11:51 +0200

    updated README.md

--- scripts/install/archlinux/postinstall/install_fonts.sh
+++ scripts/install/archlinux/postinstall/install_fonts.sh
@@ -10,5 +10,12 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 && rm Hack.zip \
 && fc-cache -fv
 
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip \
+&& cd ~/.local/share/fonts \
+&& unzip CascadiaCode.zip \
+&& rm CascadiaCode.zip \
+&& fc-cache -fv
+
 # brew install --cask font-hack-nerd-font
 # brew install --cask font-jetbrains-mono-nerd-font
+# brew install --cask font-cascadia-code-nf
