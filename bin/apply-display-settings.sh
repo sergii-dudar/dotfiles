47c904886fe19decf7c0dc2e40f36bc0a490d663
serhii.dudar <shtanga.net@gmail.com>
2024-11-08 20:37:47 +0200

    x11 scaling for my monitor resolution solution

--- bin/apply-display-settings.sh
+++ bin/apply-display-settings.sh
@@ -1,6 +1,6 @@
 if $( cat /proc/acpi/button/lid/*/state | grep -q closed )
 then
-	xrandr --output eDP-1 --off
+    xrandr --output eDP-1 --off
 fi
 
 #if $( cat /proc/acpi/button/lid/LID0/state | grep -q open )
@@ -9,3 +9,8 @@ fi
 #else
 #	xrandr --output eDP-1 --off
 #fi
+#xrandr --output HDMI-1 --primary --mode 3840x2160 --rotate normal --dpi 50
+
+# must be the same as in .Xresources
+
+xrandr --dpi 112
--- bin/change_language.sh
+++ bin/change_language.sh
@@ -4,7 +4,7 @@ CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==3{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" = "us" ]; then
     setxkbmap "ua"
-	notify-send "Lang: UA" -t 700
+    notify-send "Lang: UA" -t 700
 else
     setxkbmap "us"
     notify-send "Lang: US" -t 700
