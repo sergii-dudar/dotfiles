32afa12a42adf16f655d60bbbd61dfb79361f374
serhii.dudar <shtanga.net@gmail.com>
2024-11-09 15:44:23 +0200

    polybar

--- bin/change_language.sh
+++ bin/change_language.sh
@@ -4,8 +4,8 @@ CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==3{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" = "us" ]; then
     setxkbmap "ua"
-    notify-send "Lang: UA" -t 700
+    notify-send "Lang: UA ""🇺🇦" -t 700
 else
     setxkbmap "us"
-    notify-send "Lang: US" -t 700
+    notify-send "Lang: US ""🇺🇸" -t 700
 fi
