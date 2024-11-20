0e7d39880c5b3b58b175267682f3d9a81e2976a0
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-11-20 11:49:45 +0200

    aerospace hide all improvement

--- aerospace/.config/aerospace/scratchpad_hideall.sh
+++ aerospace/.config/aerospace/scratchpad_hideall.sh
@@ -1,22 +1,18 @@
 #!/opt/homebrew/bin/bash
 
-# TODO:
-
 set -euo pipefail
 
 CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
 
+ALL_WS_APP_NAMES=$(aerospace list-windows --workspace "$CURRENT_WORKSPACE" --format "%{app-name}")
+ALL_WS_APP_IDS=$(aerospace list-windows --workspace "$CURRENT_WORKSPACE" --format "%{window-id}%{right-padding} | '%{app-name}'")
+
 for app_name in "$@"; do
-    echo "app name: $app_name"
+    # echo "app name: $app_name"
 
-    if aerospace list-windows --workspace "$CURRENT_WORKSPACE" --format "%{app-name}" | grep "$app_name"; then
-        echo "app name matched: $app_name"
-
-        app_window_id=$(aerospace list-windows --workspace "$CURRENT_WORKSPACE" --format "%{window-id}%{right-padding} | '%{app-name}'" |
-            grep "$app_name" |
-            cut -d ' ' -f1 |
-        head -n1)
+    if echo "$ALL_WS_APP_NAMES" | grep "$app_name"; then
+        # echo "app name matched: $app_name"
+        app_window_id=$(echo "$ALL_WS_APP_IDS" | grep "$app_name" | cut -d ' ' -f1 | head -n1)
         aerospace move-node-to-workspace NSP --window-id "$app_window_id"
-
     fi
 done
