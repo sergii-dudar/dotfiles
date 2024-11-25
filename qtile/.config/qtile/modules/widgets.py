fd3dee892699514110a497e25798b0887167f4b3
serhii.dudar <shtanga.net@gmail.com>
2024-11-25 02:49:21 +0200

    qtile

--- qtile/.config/qtile/modules/widgets.py
+++ qtile/.config/qtile/modules/widgets.py
@@ -192,26 +192,47 @@ clock = widget.Clock(
     background=backgroundColor,
     mouse_callbacks={"Button1": toggle_clock_format},
 )
+
 class MouseOverClock(widget.Clock):
-    defaults = [
-        (
-            "long_format",
-            "%A %d %B %Y | %H:%M",
-            "Format to show when mouse is over widget."
-        )
-    ]
+    # defaults = [
+    #     (
+    #         "long_format",
+    #         "%A %d %B %Y | %H:%M",
+    #         "Format to show when mouse is over widget."
+    #     ),
+    #     (
+    #         "date",
+    #         "%a, %b %d",
+    #         "Format to show on click."
+    #     )
+    # ]
+    #
+    # def __init__(self, **config):
+    #     widget.Clock.__init__(self, **config)
+    #     self.add_defaults(MouseOverClock.defaults)
+    #     self.short_format = self.format
+    #
+    # def mouse_enter(self, *args, **kwargs):
+    #     self.format = self.long_format
+    #     self.bar.draw()
+    #
+    # def mouse_leave(self, *args, **kwargs):
+    #     self.format = self.short_format
+    #     self.bar.draw()
 
     def __init__(self, **config):
         widget.Clock.__init__(self, **config)
-        self.add_defaults(MouseOverClock.defaults)
-        self.short_format = self.format
+        self.formats = ["%I:%M %p", "%A, %B %d"]
+        self.current_format_index = 0
+        self.format = self.formats[self.current_format_index]
+        self.add_callbacks({
+                "Button1": self.button_press,
+            }
+        )
 
-    def mouse_enter(self, *args, **kwargs):
-        self.format = self.long_format
-        self.bar.draw()
-
-    def mouse_leave(self, *args, **kwargs):
-        self.format = self.short_format
+    def button_press(self, x, y, button):
+        self.current_format_index = (self.current_format_index + 1) % len(self.formats)
+        self.format = self.formats[self.current_format_index]
         self.bar.draw()
 
 clock_cust = MouseOverClock(
