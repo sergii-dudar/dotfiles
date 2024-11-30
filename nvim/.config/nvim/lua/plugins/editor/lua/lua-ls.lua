39bda5b50a3b780db5b53d54bf0cd3fe54835be2
serhii.dudar <shtanga.net@gmail.com>
2024-11-30 13:59:01 +0200

    nvim lua_ls

---
+++ nvim/.config/nvim/lua/plugins/editor/lua/lua-ls.lua
@@ -0,0 +1,43 @@
+return {
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            enable = true,
+                            globals = { "vim", "use", "awesome", "client", "root", "screen" },
+                            disable = { "lowercase-global" },
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                "/usr/share/nvim/runtime/lua",
+                                "/usr/share/nvim/runtime/lua/lsp",
+                                "/usr/share/awesome/lib",
+                            },
+                        },
+                        codeLens = {
+                            enable = true,
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        doc = {
+                            privateName = { "^_" },
+                        },
+                        hint = {
+                            enable = true,
+                            setType = false,
+                            paramType = true,
+                            paramName = "Disable",
+                            semicolon = "Disable",
+                            arrayIndex = "Disable",
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
