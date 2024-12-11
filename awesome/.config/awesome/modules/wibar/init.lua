5dc6e455fea060755404f786d59b9687ddbddd9f
serhii.dudar <shtanga.net@gmail.com>
2024-12-11 20:59:14 +0200

    awesome fix hide bar key

--- awesome/.config/awesome/modules/wibar/init.lua
+++ awesome/.config/awesome/modules/wibar/init.lua
@@ -22,93 +22,93 @@ M.setup = function(opts)
 
     awful.screen.connect_for_each_screen(function(s)
         -- Add widgets to the wibox
-        awful
-            .wibar({
-                position = "top",
-                screen = s,
-                height = 35,
-            })
-            :setup({
-                layout = wibox.layout.align.horizontal,
-                expand = "none",
+        s.mywibox = awful.wibar({
+            position = "top",
+            screen = s,
+            height = 35,
+        })
+
+        s.mywibox:setup({
+            layout = wibox.layout.align.horizontal,
+            expand = "none",
+            {
+                widget = wibox.container.margin,
+                bottom = 2,
+                top = 2,
+                {
+                    widget = wibox.container.place,
+                    layout = wibox.layout.fixed.horizontal,
+                    --spacing = 5,
+                    appmenu_widget.applications,
+                    simple_widget.separator,
+                    --wibox.container.margin(launcher.mylauncher, 5, 0, 0, 0),
+                    --separator,
+                    layout_widget.layoutbox_with_name(s),
+                    simple_widget.separator,
+
+                    -- Create a promptbox for each screen
+                    awful.widget.prompt(),
+                    simple_widget.tasklist(s, opts),
+                    --simple_widget.separator,
+                },
+            },
+
+            {
+                widget = wibox.container.place,
+                layout = wibox.layout.fixed.horizontal,
+                --spacing = 15,
+                date_widgets.date,
+                simple_widget.space,
+                simple_widget.space,
+                taglist_widget.setup(s, opts),
+                simple_widget.space,
+                simple_widget.space,
+                date_widgets.time,
+            },
+
+            { -- Right widgets
+
+                widget = wibox.container.place,
+                h_align = "right",
                 {
                     widget = wibox.container.margin,
-                    bottom = 2,
-                    top = 2,
+                    right = 5,
+                    --bottom = 1,
+                    --top = 1,
                     {
                         widget = wibox.container.place,
                         layout = wibox.layout.fixed.horizontal,
                         --spacing = 5,
-                        appmenu_widget.applications,
-                        simple_widget.separator,
-                        --wibox.container.margin(launcher.mylauncher, 5, 0, 0, 0),
-                        --separator,
-                        layout_widget.layoutbox_with_name(s),
-                        simple_widget.separator,
+                        table.unpack(common_util.concat_match_tables({
+                            keyboard_layout_widget.setup(),
+                            simple_widget.separator,
+                            volume_widget.setup(opts),
+                            simple_widget.separator,
+                        }, {
+                            is_match = function()
+                                return common_util.directory_exists("/sys/class/power_supply/BAT0")
+                            end,
+                            table = {
+                                -- add this widgets only if battery present
+                                battery_widget.battery,
+                                simple_widget.separator,
+                            },
+                        }, {
+                            system_widget.mem,
+                            simple_widget.separator,
+                            system_widget.cpu,
+                            simple_widget.separator,
+                            system_widget.fs,
+                            simple_widget.separator,
 
-                        -- Create a promptbox for each screen
-                        awful.widget.prompt(),
-                        simple_widget.tasklist(s, opts),
-                        --simple_widget.separator,
+                            --wibox.container.margin(my_widget, left, right, top, bottom)
+                            wibox.container.margin(wibox.widget.systray(), 0, 0, 6, 6),
+                            wibox.container.margin(powermenu_widget.powermenu, 10, 0, 0, 0),
+                        })),
                     },
                 },
-
-                {
-                    widget = wibox.container.place,
-                    layout = wibox.layout.fixed.horizontal,
-                    --spacing = 15,
-                    date_widgets.date,
-                    simple_widget.space,
-                    simple_widget.space,
-                    taglist_widget.setup(s, opts),
-                    simple_widget.space,
-                    simple_widget.space,
-                    date_widgets.time,
-                },
-
-                { -- Right widgets
-
-                    widget = wibox.container.place,
-                    h_align = "right",
-                    {
-                        widget = wibox.container.margin,
-                        right = 5,
-                        --bottom = 1,
-                        --top = 1,
-                        {
-                            widget = wibox.container.place,
-                            layout = wibox.layout.fixed.horizontal,
-                            --spacing = 5,
-                            table.unpack(common_util.concat_match_tables({
-                                keyboard_layout_widget.setup(),
-                                simple_widget.separator,
-                                volume_widget.setup(opts),
-                                simple_widget.separator,
-                            }, {
-                                is_match = function()
-                                    return common_util.directory_exists("/sys/class/power_supply/BAT0")
-                                end,
-                                table = {
-                                    -- add this widgets only if battery present
-                                    battery_widget.battery,
-                                    simple_widget.separator,
-                                },
-                            }, {
-                                system_widget.mem,
-                                simple_widget.separator,
-                                system_widget.cpu,
-                                simple_widget.separator,
-                                system_widget.fs,
-                                simple_widget.separator,
-
-                                --wibox.container.margin(my_widget, left, right, top, bottom)
-                                wibox.container.margin(wibox.widget.systray(), 0, 0, 6, 6),
-                                wibox.container.margin(powermenu_widget.powermenu, 10, 0, 0, 0),
-                            })),
-                        },
-                    },
-                },
-            })
+            },
+        })
     end)
 end
 
