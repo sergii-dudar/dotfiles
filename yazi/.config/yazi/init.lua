f750450408d38760b66badc24cb5035e59821f6a
serhii.dudar <shtanga.net@gmail.com>
2024-12-25 00:45:03 +0200

    yazi config

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -19,3 +19,24 @@ require("copy-file-contents"):setup({
 })
 
 require("system-clipboard")
+
+-- https://github.com/sxyazi/yazi/blob/shipped/yazi-plugin/preset/components/status.lua
+function Status:mode()
+    local mode = tostring(self._tab.mode):upper()
+
+    local style = self:style()
+    return ui.Line({
+        ui.Span(THEME.status.separator_open):fg(style.main.bg),
+        ui.Span(" " .. mode .. " "):style(style.main),
+        ui.Span(THEME.status.separator_close):fg(style.main.bg):bg(style.alt.bg),
+    })
+end
+
+function Status:name()
+    local h = self._current.hovered
+    if not h then
+        return ""
+    end
+
+    return " 👉 " .. h.name:gsub("\r", "?", 1)
+end
