5ea49c9f83242848bf2a9c40aab71e8c966fd80f
serhii.dudar <shtanga.net@gmail.com>
2024-12-27 20:21:22 +0200

    xmonad

--- zsh/serhii.shell/variables.sh
+++ zsh/serhii.shell/variables.sh
@@ -32,6 +32,7 @@ else
     # in case commented nvm (as slow)
     export PATH=$PATH:$HOME/.nvm/versions/node/v20.17.0/bin
     export PATH=$PATH:$HOME/.ghcup/bin
+    export PATH=$PATH:$HOME/.local/bin
 
     export QT_SCALE_FACTOR=1.75
     export LC_CTYPE=en_US.UTF-8
