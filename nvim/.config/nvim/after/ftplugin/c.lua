7bb3bb525117d00634ba30fe47096f982509c84e
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-12-30 20:37:17 +0200

    c ftp role

---
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,7 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 8 --8, need create custom indentexpr to handle correctly where 8, and where 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
