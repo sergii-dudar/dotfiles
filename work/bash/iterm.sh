57d5cb5e5e95fd8c81d0d18d7d8a8aa275c65cd0
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-02-11 14:54:59 +0200

    changes

--- work/bash/iterm.sh
+++ work/bash/iterm.sh
@@ -5,7 +5,7 @@
 
 #osascript -e 'do shell script "open -a Kitty --args --directory ~/dotfiles"'
 #osascript -e 'do shell script "kitty --directory ~/dotfiles"'
-open -a Kitty
+# open -a Kitty
 
 #
 #sascript -e 'tell application "Kitty" to create window --args --directory "$(pwd)"'
