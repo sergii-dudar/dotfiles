f45501f36eaef42da3fb38774003fbdfeb6832df
serhii.dudar <shtanga.net@gmail.com>
2025-02-23 20:17:57 +0200

    dwmblocks improvements

--- suckless/dwmblocks-async/config.h
+++ suckless/dwmblocks-async/config.h
@@ -18,19 +18,19 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-key-layout", 0, 1) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-volume", 0, 2) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-battery", 300, 3) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-memory", 30, 4) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-cpu-load", 30, 5) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-cpu-temp", 120, 6) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-disk", 1200, 7) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-weather", 1200, 8) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-date", 60, 10) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner 'env XDG_CURRENT_DESKTOP=GNOME gnome-control-center' '' '#3071db'", 0, 11) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner '/home/serhii/.config/rofi/scripts/launcher_t1' '󰣇' '#bd93f9'", 0, 12) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner 'env QT_SCALE_FACTOR=1.4 qbittorrent' '󱑤' '#8caaee'", 0, 13) \
-    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner '/home/serhii/.config/rofi/scripts/powermenu_t1' '⏻' '#d35f5e'", 0, 14)
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-date",          0,     1) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-key-layout",    0,      2) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-volume",        0,      3) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-battery",       300,    4) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-memory",        30,     5) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-cpu-load",      30,     6) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-cpu-temp",      120,    7) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-disk",          1200,   8) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-weather",       1200,   9) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner 'env XDG_CURRENT_DESKTOP=GNOME gnome-control-center' '' '#3071db'",     0, 11) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner '/home/serhii/.config/rofi/scripts/launcher_t1' '󰣇' '#bd93f9'",          0, 12) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner 'env QT_SCALE_FACTOR=1.4 qbittorrent' '󱑤' '#8caaee'",                    0, 13) \
+    X("", "/home/serhii/dotfiles/bin/dwm/sb-runner '/home/serhii/.config/rofi/scripts/powermenu_t1' '⏻' '#d35f5e'",         0, 14)
 #endif  // CONFIG_H
 
 // update from sh: kill -(34 + signal) $(pidof dwmblocks)
