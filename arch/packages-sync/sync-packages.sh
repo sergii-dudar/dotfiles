b1e1c7a807facf02a882f311ea216f17b952651a
serhii.dudar <shtanga.net@gmail.com>
2025-05-07 18:48:07 +0300

    changes

--- arch/packages-sync/sync-packages.sh
+++ arch/packages-sync/sync-packages.sh
@@ -2,5 +2,5 @@
 
 info_path=~/dotfiles/arch/packages-sync
 
-sudo pacman -S --needed - < "$info_path"/pkglist.txt
+yay -S --needed - < "$info_path"/pkglist.txt
 yay -S --needed - < "$info_path"/aurlist.txt
