255746bb9ea00c36d9bacc88a80c2c0b94c2fa85
serhii.dudar <shtanga.net@gmail.com>
2025-05-08 12:20:23 +0300

    changed colorschema to growbox

--- nvim/.config/nvim/lua/plugins/ui/color-schema.lua
+++ nvim/.config/nvim/lua/plugins/ui/color-schema.lua
@@ -1,4 +1,5 @@
 return {
+    -- ##############  FAVORITE ###################
     {
         "catppuccin/nvim",
         name = "catppuccin",
@@ -17,27 +18,70 @@ return {
           transparent_background = true
       }]]
     },
-    --[[{
-      'rose-pine/neovim',
-      name = 'rose-pine',
-      priority = 1000,
-      optional = false,
-      lazy = false,
-      opts = { highlight_groups = { Visual = { bg = '#373267' } } },
-  },]]
-    --[[{
-      "rebelot/kanagawa.nvim",
-      priority = 1000
-  },]]
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         -- colorscheme = "catppuccin-mocha",
+    --         -- colorscheme = "gruvbox",
+    --     },
+    -- },
+    -- ############## MOVE EYE (Monokai, Gruvbox, Everforest) ###################
+    -- {
+    --     "sainnhe/everforest",
+    --     lazy = false,
+    --     optional = false,
+    --     priority = 1000,
+    --     config = function()
+    --         -- Everforest is a green based color scheme; it's designed to be warm and soft in order to protect developers' eyes.
+    --
+    --         -- Optionally configure and load the colorscheme
+    --         -- directly inside the plugin declaration.
+    --
+    --         vim.g.everforest_enable_italic = true
+    --         vim.g.everforest_background = "hard" -- `'hard'`, `'medium'`, `'soft'`
+    --         -- vim.cmd("set background=dark") -- dark,  light
+    --         vim.cmd.colorscheme("everforest")
+    --     end,
+    -- },
+    -- {
+    --     "navarasu/onedark.nvim",
+    --     priority = 1000,
+    --     optional = false,
+    --     lazy = false,
+    --     config = function()
+    --         -- +++
+    --         -- https://github.com/navarasu/onedark.nvim
+    --         require("onedark").setup({
+    --             style = "warmer", -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
+    --         })
+    --         -- Enable theme
+    --         require("onedark").load()
+    --     end,
+    -- },
     {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin-mocha",
-        },
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        optional = false,
+        lazy = false,
+        config = function()
+            require("gruvbox").setup({
+                contrast = "hard", -- can be "hard", "soft" or empty string
+            })
+
+            -- vim.o.background = "dark" -- or "light" for light mod
+            vim.cmd.colorscheme("gruvbox")
+        end,
     },
-    --{
-    --  "sainnhe/sonokai",
-    --  priority = 1000, -- Ensure it loads first
-    --}
-    --{ "ellisonleao/gruvbox.nvim", priority = 1000}
+    -- {
+    --     "tanvirtin/monokai.nvim",
+    --     priority = 1000,
+    --     optional = false,
+    --     lazy = false,
+    --     config = function()
+    --         require("monokai").setup({})
+    --         -- require("monokai").setup({ palette = require("monokai").pro })
+    --         -- require("monokai").setup({ palette = require("monokai").soda })
+    --         -- require("monokai").setup({ palette = require("monokai").ristretto })
+    --     end,
+    -- },
 }
