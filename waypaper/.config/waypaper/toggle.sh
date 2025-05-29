567b669d43445d634f98dd30f22ee5e553f3be61
serhii.dudar <shtanga.net@gmail.com>
2025-05-30 00:38:40 +0300

    config

---
+++ waypaper/.config/waypaper/toggle.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+wm_name="${1:-}"
+
+if pgrep waypaper; then
+    killall waypaper
+    exit 0
+fi
+
+case "$wm_name" in
+    sway)
+
+        ;;
+    hyprland)
+        # first_monitor=$(hyprctl monitors | grep "Monitor " | head -n 1 | awk '{ print $2 }')
+        waypaper --backend hyprpaper
+        ;;
+    *)
+        ;;
+esac
