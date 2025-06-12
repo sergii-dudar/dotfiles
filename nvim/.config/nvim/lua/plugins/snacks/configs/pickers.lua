7d17a1338a6c0e881c68d8b26523dec0e44fe570
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-06-12 11:51:41 +0300

    nvim snacks refactoring

---
+++ nvim/.config/nvim/lua/plugins/snacks/configs/pickers.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+Snacks.util.set_hl({
+    PathHidden = "",
+}, { prefix = "SnacksPicker", default = true })
+
+local layouts = require("plugins.snacks.configs.layouts")
+
+M.picker = {
+    -- layout = {
+    --     cycle = false,
+    -- },
+    layout = layouts.custom_default,
+    hidden = true, -- Include hidden files in grep
+    ignored = true, -- Exclude git-ignored files
+    formatters = {
+        file = {
+            filename_first = true, -- display filename before the file path
+            filename_only = false, -- only show the filename
+            truncate = 250, -- truncate the file path to (roughly) this length
+        },
+    },
+    sources = {
+        explorer = {
+            layout = layouts.custom_explorer,
+            auto_close = false,
+        },
+        files = {
+            -- cmd = "fd", -- "fd"| "rg"| "find" command to use. Leave empty to auto-detect
+            cmd = "fd",
+            hidden = true, -- Show hidden files
+            ignored = true, -- Exclude git-ignored files
+            -- exclude = { "node_modules/*", "*.pyc", "*.log" }, -- Exclude patterns
+            -- preview = false, -- Enable file preview in picker
+        },
+        grep = {
+            hidden = true, -- Show hidden files
+            ignored = true, -- Exclude git-ignored files
+        },
+    },
+    toggles = {
+        hidden = "󱞞",
+        ignored = "",
+        modified = "",
+    },
+}
+M.explorer = {
+    hidden = true, -- Show hidden files
+    ignored = true, -- Show git-ignored files
+    replace_netrw = true,
+    auto_close = false, -- Keep explorer open
+}
+
+return M
--- nvim/.config/nvim/lua/plugins/snacks/init.lua
+++ nvim/.config/nvim/lua/plugins/snacks/init.lua
@@ -14,59 +14,13 @@ return {
             -- vim.schedule(function()
             --     vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "SnacksPickerPathNormal" })
             -- end)
-            Snacks.util.set_hl({
-                PathHidden = "",
-            }, { prefix = "SnacksPicker", default = true })
-
-            local layouts = require("plugins.snacks.configs.layouts")
+            local pickers = require("plugins.snacks.configs.pickers")
             return vim.tbl_deep_extend("force", opts or {}, {
                 zen = {
                     win = { style = "zen", width = 0.85 },
                 },
-                picker = {
-                    -- layout = {
-                    --     cycle = false,
-                    -- },
-                    layout = layouts.custom_default,
-                    hidden = true, -- Include hidden files in grep
-                    ignored = true, -- Exclude git-ignored files
-                    formatters = {
-                        file = {
-                            filename_first = true, -- display filename before the file path
-                            filename_only = false, -- only show the filename
-                            truncate = 250, -- truncate the file path to (roughly) this length
-                        },
-                    },
-                    sources = {
-                        explorer = {
-                            layout = layouts.custom_explorer,
-                            auto_close = false,
-                        },
-                        files = {
-                            -- cmd = "fd", -- "fd"| "rg"| "find" command to use. Leave empty to auto-detect
-                            cmd = "fd",
-                            hidden = true, -- Show hidden files
-                            ignored = true, -- Exclude git-ignored files
-                            -- exclude = { "node_modules/*", "*.pyc", "*.log" }, -- Exclude patterns
-                            -- preview = false, -- Enable file preview in picker
-                        },
-                        grep = {
-                            hidden = true, -- Show hidden files
-                            ignored = true, -- Exclude git-ignored files
-                        },
-                    },
-                    toggles = {
-                        hidden = "󱞞",
-                        ignored = "",
-                        modified = "",
-                    },
-                },
-                explorer = {
-                    hidden = true, -- Show hidden files
-                    ignored = true, -- Show git-ignored files
-                    replace_netrw = true,
-                    auto_close = false, -- Keep explorer open
-                },
+                picker = pickers.picker,
+                explorer = pickers.explorer,
                 image = {
                     force = true,
                     enabled = true,
@@ -83,7 +37,7 @@ return {
                 },
                 scroll = { enabled = false },
                 -- bigfile = { enabled = true },
-                -- dashboard = { enabled = true },
+                dashboard = { enabled = true },
                 -- indent = { enabled = true },
                 -- input = { enabled = true },
                 -- notifier = { enabled = true },
