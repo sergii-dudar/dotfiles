141123388c724961916eb0eea733b1e38c90ef70
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-06-13 11:58:18 +0300

    nvim dashboard

--- nvim/.config/nvim/lua/plugins/snacks/configs/dashboards.lua
+++ nvim/.config/nvim/lua/plugins/snacks/configs/dashboards.lua
@@ -17,8 +17,16 @@ M.default = {
     sections = {
         { section = "header", padding = { 0, 0 } },
         { icon = " ", title = "Keymaps: ", section = "keys", indent = 3, padding = { 1, 0 } },
-        { icon = " ", title = "Recent Files: ", section = "recent_files", indent = 3, padding = { 1, 0 } },
-        { icon = " ", title = "Projects: ", section = "projects", indent = 3, padding = { 1, 0 } },
+        {
+            icon = " ",
+            title = "Recent Files: ",
+            section = "recent_files",
+            limit = 20,
+            cwd = true,
+            indent = 3,
+            padding = { 1, 0 },
+        },
+        -- { icon = " ", title = "Projects: ", section = "projects", indent = 3, padding = { 1, 0 } },
         -- { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
         -- { section = "terminal", cmd = "fortune -s", ttl = 0, hl = "header", padding = 1, indent = 8 },
         -- {
