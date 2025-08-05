07958dd4b66e372fbd94f580753eb1f7be22e3da
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-08-05 20:32:58 +0300

    changes

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -4,7 +4,9 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on background.color=0x44ffffff
+elif [ $(aerospace list-windows --workspace "$1" | wc -l | awk '{print $1}') -gt 0 ] && [ "$1" != "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.drawing=on background.color=0xffa6da95
 else
     sketchybar --set $NAME background.drawing=off
 fi
