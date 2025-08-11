c116cb7a26e4f490410b3ad307ec0c6b7404a2c5
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-08-11 12:35:24 +0300

    macos, size_and_center_float.sh: add support of presense second vertical monitor

--- aerospace/.config/aerospace/size_and_center_float.sh
+++ aerospace/.config/aerospace/size_and_center_float.sh
@@ -2,7 +2,12 @@
 
 APP_NAME=$1
 
-osascript -l JavaScript -e '
+#SCREEN_NUMBERS=$(system_profiler SPDisplaysDataType | rg "Resolution:" | wc -l | awk '{print $1}')
+SCREEN_NUMBERS=$(system_profiler SPDisplaysDataType | rg --count "Resolution:")
+
+if [ "$SCREEN_NUMBERS" = 1 ]; then
+    # Single 4k monitor suport
+    osascript -l JavaScript -e '
 var frontAppName = "'"$APP_NAME"'";
 var frontApp = Application(frontAppName);
 var topWindow = frontApp.windows[0];
@@ -18,4 +23,28 @@ windowBounds.x = (desktopBounds.width - windowBounds.width) / 2 + desktopBounds.
 windowBounds.y = (desktopBounds.height - windowBounds.height) / 2 + desktopBounds.y;
 
 topWindow.bounds = windowBounds;
-'
+    '
+else
+    osascript -l JavaScript -e '
+var frontAppName = "'"$APP_NAME"'";
+var frontApp = Application(frontAppName);
+var topWindow = frontApp.windows[0];
+
+var windowBounds = topWindow.bounds();
+
+var desktopBounds = Application("Finder").desktop.window().bounds();
+
+var desktopWidth = desktopBounds.width - 1440; // remove second vert monitor width to make correct position on first monitor.
+var desktopHeight = desktopBounds.height;
+
+windowBounds.width = desktopWidth * 0.8;
+windowBounds.height = desktopHeight * 0.5;
+
+windowBounds.x = (desktopHeight - windowBounds.width) / 2 + desktopBounds.x;
+windowBounds.y = (desktopHeight - windowBounds.height) / 2 + desktopBounds.y;
+
+topWindow.bounds = windowBounds;
+    '
+
+fi
+
