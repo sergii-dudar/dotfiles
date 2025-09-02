5c71684976ac06d195f5a006c861f57cdccfed15
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-09-02 15:20:01 +0300

    gitconfig rework, conflict resolution based on diffview etc

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -24,17 +24,6 @@ autocmd("TermOpen", {
     end,
 })
 
-vim.api.nvim_create_autocmd("User", {
-    pattern = "GitConflictDetected",
-    callback = function()
-        vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
-        vim.keymap.set("n", "cww", function()
-            engage.conflict_buster()
-            create_buffer_local_mappings()
-        end)
-    end,
-})
-
 -- highlight yanks
 autocmd("TextYankPost", {
     group = yank_group,
