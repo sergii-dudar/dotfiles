dbe8e1e7cbf770c49559c097f736e7fb444c6057
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-09-02 14:55:02 +0300

    nvim: nvimdiff added as git conflicts resolver

---
+++ nvim/.config/nvim/lua/plugins/editor/git.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        keys = {
+            { "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle Line Blame" },
+        },
+    },
+    {
+        "sindrets/diffview.nvim",
+        version = "*",
+        config = true,
+    },
+    -- {
+    --     "akinsho/git-conflict.nvim",
+    --     version = "*",
+    --     config = true,
+    -- },
+}
--- nvim/.config/nvim/lua/plugins/editor/gitsigns.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "lewis6991/gitsigns.nvim",
-    keys = {
-        { "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle Line Blame" },
-    },
-}
