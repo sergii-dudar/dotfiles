f0d6465d0adecf0015847e030379424ff8b09e43
Serhii Dudar <shtanga.net@gmail.com>
2025-09-03 12:43:02 +0300

    changes

--- nvim/.config/nvim/lua/utils/neo-tree-util.lua
+++ nvim/.config/nvim/lua/utils/neo-tree-util.lua
@@ -18,7 +18,7 @@ M.is_enable_neo_tree = function()
     -- load neotree only to lister dirs, and disable for direct file[s] open (like `nvim [files...]`)
     -- vim.notify("result 1: " .. tostring(list_util.any_match(current_file, open_if_dir_contains)), vim.log.levels.INFO)
     -- vim.notify("result 2: " .. tostring(vim.fn.argc()), vim.log.levels.INFO)
-    return list_util.any_match(current_file, open_if_dir_contains) and vim.fn.argc() == 0
+    return list_util.any_match(current_file, open_if_dir_contains) -- and vim.fn.argc() == 0
 end
 
 return M
