ad49d78f304623bda583d969b9b15bfa202e5512
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-10-17 13:29:28 +0300

    changes

---
+++ bin/install/configs/macos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo ln -s ~/dotfiles/keyboard/kanata/macos ~/.config/kanata                                                                                                                                        │
+sudo ln -s ~/dotfiles/mpd-config/mpd-osx ~/.mpd                                                                                                                                                     │
+sudo ln -s ~/dotfiles/mpd-config/rmpc ~/.config/rmpc                                                                                                                                           │
+sudo ln -s ~/dotfiles/mpd-config/ncmpcpp ~/.config/ncmpcpp                                                                                                                                     │
+stow lazygit                                                                                                                                                                                   │
+stow kitty                                                                                                                                                                                     │
+stow karabiner                                                                                                                                                                                 │
+stow k9s                                                                                                                                                                                       │
+stow janky-borders                                                                                                                                                                             │
+stow ghostty                                                                                                                                                                                   │
+stow fastfetch                                                                                                                                                                                 │
+stow btop                                                                                                                                                                                      │
+stow bat                                                                                                                                                                                       │
+stow aerospace                                                                                                                                                                                 │
+stow ranger                                                                                                                                                                                    │
+stow sketchybar                                                                                                                                                                                │
+stow starship                                                                                                                                                                                  │
+stow tmux-powerline                                                                                                                                                                            │
+stow wezterm                                                                                                                                                                                   │
+stow yazi                                                                                                                                                                                      │
+stow nvim
