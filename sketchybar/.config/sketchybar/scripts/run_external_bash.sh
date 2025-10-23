82d820e68b3734aa4edaf0f7e2796569f9662d16
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-10-23 20:09:10 +0300

    much improved osx sketchybar run_external_bash.sh script (without duplication iterm instances)

--- sketchybar/.config/sketchybar/scripts/run_external_bash.sh
+++ sketchybar/.config/sketchybar/scripts/run_external_bash.sh
@@ -2,12 +2,41 @@
 
 COMMAND=$1
 # COMMAND=/opt/homebrew/bin/btop
-osascript -e 'tell application "iTerm"
-    activate
-    set newWindow to (create window with default profile)
-    tell current session of newWindow
-        write text "'"$COMMAND"'"
+# osascript -e 'tell application "iTerm"
+#     activate
+#     set newWindow to (create window with default profile)
+#     tell current session of newWindow
+#         write text "'"$COMMAND"'"
+#     end tell
+# end tell'
+osascript -e '
+if application "iTerm" is running then
+    tell application "iTerm"
+        set newWindow to (create window with default profile)
+        tell current session of newWindow
+            write text "'"$COMMAND"'"
+        end tell
+        activate
     end tell
-end tell'
+else
+    tell application "iTerm"
+        activate
+        -- Wait for window 1 to exist (timeout after ~5 seconds)
+        repeat 50 times
+            if (exists window 1) then exit repeat
+            delay 0.1
+        end repeat
+        -- Wait for session 1 to exist
+        repeat 50 times
+            if (exists current session of window 1) then exit repeat
+            delay 0.1
+        end repeat
+        tell current session of window 1
+            write text "'"$COMMAND"'"
+        end tell
+    end tell
+end if
+'
+
 
 ~/.config/aerospace/size_and_center_float.sh "iTerm"
