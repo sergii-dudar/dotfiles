483e37f7de0e3315e2f4f834ccdc18c38aec9d61
Serhii Dudar <shtanga.net@gmail.com>
2025-10-30 01:09:26 +0200

    nvim ai

--- nvim/.config/nvim/lua/plugins/editor/ai.lua
+++ nvim/.config/nvim/lua/plugins/editor/ai.lua
@@ -1,11 +1,28 @@
 return {
     {
         "folke/sidekick.nvim",
+        keys = {
+            { "<a-a>", false },
+            {
+                "<leader>aa",
+                function()
+                    require("sidekick.cli").toggle({ name = "copilot", focus = true })
+                end,
+                desc = "Toggle Copilot CLI",
+            },
+        },
         opts = {
             debug = true,
+            nes = {
+                enabled = false, -- disable Next Edit Suggestions as I don't like it, I need only CLI AI Chat
+            },
             cli = {
-                -- picker = "fzf-lua",
-                -- picker = "snacks",
+                picker = "snacks",
+                win = {
+                    keys = {
+                        files = { "<c-g>", "files", mode = "nt", desc = "open file picker" },
+                    },
+                },
                 mux = {
                     -- backend = vim.env.ZELLIJ and "zellij" or "tmux", -- default to tmux unless zellij is detected
                     backend = "tmux",
@@ -14,23 +31,13 @@ return {
                     -- window: when run inside a terminal multiplexer, new sessions will be created in a new tab
                     -- split: when run inside a terminal multiplexer, new sessions will be created in a new split
                     -- NOTE: zellij only supports `terminal`
-                    create = "window", ---@type "terminal"|"window"|"split"
+                    create = "terminal", ---@type "terminal"|"window"|"split"
                     split = {
                         vertical = true, -- vertical or horizontal split
                         size = 0.5, -- size of the split (0-1 for percentage)
                     },
                 },
-                debug = true,
-                cli = {
-                    -- picker = "fzf-lua",
-                    -- win = { layout = "float" },
-                    tools = {
-                        debug = {
-                            -- print env and read -p "any key to continue"
-                            cmd = { "bash", "-c", "env | sort | bat -l env" },
-                        },
-                    },
-                },
+                debug = false,
             },
         },
     },
