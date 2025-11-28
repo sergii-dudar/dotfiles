5900088c01c936775f957e9339bbde349571ac23
Serhii Dudar <shtanga.net@gmail.com>
2025-11-28 14:30:14 +0200

    nvim: relative current line num hl

--- nvim/.config/nvim/lua/plugins/ui/color-schema.lua
+++ nvim/.config/nvim/lua/plugins/ui/color-schema.lua
@@ -51,12 +51,15 @@ return {
             -- vim.g.gruvbox_material_cursor = "green" -- `'auto'`, `'red'`, `'orange'`, `'yellow'`, `'green'`, `'aqua'`, `'blue'`, `'purple'`
             vim.g.gruvbox_material_float_style = "blend" -- `'bright'`, `'dim'`, `'blend'`
             vim.g.gruvbox_material_statusline_style = "default" -- `'default'`, `'mix'`, `'original'`
+            -- vim.g.gruvbox_material_sign_column_background = "grey"
             -- vim.g.gruvbox_material_ui_contrast = "low"
             -- vim.g.gruvbox_material_colors_override = {
             --   bg0 = { "#1d2021", "234" },
             --   bg2 = { "#282828", "235" },
             -- }
+            -- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#FF0000', bg = 'NONE', bold = true })
             vim.cmd.colorscheme("gruvbox-material")
+            vim.api.nvim_set_hl(0, "CursorLineNr", { link = "GreenBold" })
         end,
     },
     -- {
