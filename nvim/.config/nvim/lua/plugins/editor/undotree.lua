7b84f50780e807021f09bc1a3f8a084a1b23e395
Serhii Dudar <shtanga.net@gmail.com>
2025-12-18 01:10:04 +0200

    changes

--- nvim/.config/nvim/lua/plugins/editor/undotree.lua
+++ nvim/.config/nvim/lua/plugins/editor/undotree.lua
@@ -1,6 +1,17 @@
 return {
     "mbbill/undotree",
     keys = {
-        { "<leader>uu", vim.cmd.UndotreeToggle, desc = "Undotree Toggle" },
+        {
+            "<leader>uu",
+            function()
+                local before = vim.api.nvim_get_current_win()
+                vim.cmd.UndotreeToggle()
+                local after = vim.api.nvim_get_current_win()
+                if before == after then
+                    vim.cmd("wincmd w")
+                end
+            end,
+            desc = "Undotree Toggle",
+        },
     },
 }
