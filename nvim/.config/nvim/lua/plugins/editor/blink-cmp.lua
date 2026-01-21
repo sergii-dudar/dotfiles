9ee37fd7edcbb8365512c5c7d0e769bb56ebaa74
Serhii Dudar <shtanga.net@gmail.com>
2026-01-21 21:52:12 +0200

    nvim: changes

--- nvim/.config/nvim/lua/plugins/editor/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/editor/blink-cmp.lua
@@ -185,10 +185,9 @@ return {
 
                         opts = {
                             -- Required: path to mapstruct-path-explorer.jar
-                            jar_path = "~/serhii.home/personal/git/mapstruct-path-explorer/target/mapstruct-path-explorer.jar",
+                            -- jar_path = "~/serhii.home/personal/git/mapstruct-path-explorer/target/mapstruct-path-explorer.jar",
                             -- jar_path = "~/serhii.home/git/mapstruct-path-explorer/target/mapstruct-path-explorer.jar",
-                            -- jar_path = "~/tools/java-extensions/mapstruct/mapstruct-path-explorer.jar",
-                            -- jar_path = "~/tools/java-extensions/mapstruct/mapstruct-path-explorer.jar",
+                            jar_path = "~/tools/java-extensions/mapstruct/mapstruct-path-explorer.jar",
 
                             -- Optional: use jdtls classpath (default: true)
                             use_jdtls_classpath = true,
