7faa2011735004ede730e76c9fa3bc9e13b93ab0
Serhii Dudar <shtanga.net@gmail.com>
2026-01-23 01:34:23 +0200

    change

--- nvim/.config/nvim/lua/plugins/editor/java/java-tests.lua
+++ nvim/.config/nvim/lua/plugins/editor/java/java-tests.lua
@@ -32,6 +32,8 @@ return {
             -- { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
             -- { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
             -- { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
+            -- stylua: ignore
+            { "<leader>tR", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug Nearest" },
             {
                 "<leader>ts",
                 function()
