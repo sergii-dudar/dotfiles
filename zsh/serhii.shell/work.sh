a477b664bc26078bc36cb5710ded558cb23c6340
IUADA144 <serhii.dudar@raiffeisen.ua>
2026-02-04 13:32:23 +0200

    alacritty config update

--- zsh/serhii.shell/work.sh
+++ zsh/serhii.shell/work.sh
@@ -14,6 +14,10 @@ alias k9suat_m='k9s -n payments-uat --context aws-avalaunch-dev -c configmaps'
 alias k9sprod_m='k9s -n payments-prod --context aws-avalaunch-prod -c configmaps --readonly'
 alias k9sprod_m_rw='k9s -n payments-prod --context aws-avalaunch-prod -c configmaps'
 
+alias k9sdatadev='k9s -n payments-data-dev --context aws-avalaunch-dev -c pods'
+alias k9sdatauat='k9s -n payments-data-uat --context aws-avalaunch-dev -c pods'
+alias k9sdataprod='k9s -n payments-data-prod --context aws-avalaunch-prod -c pods'
+
 alias service_versions="list-service-versions.sh | fzf"
 
 export PATH="$PATH:$HOME/serhii.home/work/git.tools/ua-payments-tools/tools/k8s"
