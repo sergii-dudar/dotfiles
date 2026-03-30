414cb1e7314370081f8214693ad057d3072b28fb
IUADA144 <serhii.dudar@raiffeisen.ua>
2026-03-30 11:37:05 +0300

    fzf: configuration chagnes

--- zsh/serhii.shell/fzf.scripts.sh
+++ zsh/serhii.shell/fzf.scripts.sh
@@ -207,21 +207,23 @@ function findf_src() {
 # }
 
 function grept() {
+
     rm -f /tmp/rg-fzf-{r,f}
 RG_PREFIX="rg -g '!node_modules*' -g '!target*' -g '!bin*' --column --line-number --no-heading --color=always --smart-case "
 INITIAL_QUERY="${*:-}"
 fzf --ansi --disabled --query "$INITIAL_QUERY" \
     --bind "start:reload:$RG_PREFIX {q}" \
     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-    --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ ripgrep ]] &&
-echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
-echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
+    --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ rg ]] &&
+echo "rebind(change)+change-prompt(rg ❯ )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
+echo "unbind(change)+change-prompt(fzf ❯ )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
     --color "hl:-1:underline,hl+:-1:underline:reverse" \
-    --prompt '1. ripgrep> ' \
+    --prompt 'rg ❯ ' \
     --delimiter : \
+    --header-first \
     --header 'CTRL-T: Switch between ripgrep/fzf' \
-    --preview 'bat --color=always {1} --highlight-line {2}' \
-    --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+    --preview 'bat --style=changes --color=always {1} --highlight-line {2}' \
+    --preview-window 'right,60%,+{2}/3' \
     --bind 'enter:become(LIMITED=Y nvim {1} +{2})'
 }
 
