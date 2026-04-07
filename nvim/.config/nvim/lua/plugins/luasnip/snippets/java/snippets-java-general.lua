8bc7ba8e3baa18eea43bc6405b11dcb3e5f07d7f
Serhii Dudar <shtanga.net@gmail.com>
2026-04-08 01:38:00 +0300

    nvim: fix java static imports for inner class declarations

--- nvim/.config/nvim/lua/plugins/luasnip/snippets/java/snippets-java-general.lua
+++ nvim/.config/nvim/lua/plugins/luasnip/snippets/java/snippets-java-general.lua
@@ -23,12 +23,56 @@ function M.snippets()
     -- local conds = require("luasnip.extras.conditions")
     -- local conds_expand = require("luasnip.extras.conditions.expand")
 
+    local function extract_builder_type(context)
+        return context:match("(%u%w+)%s+%w+%s*=%s*$")
+    end
+
+    local function show_builder_context(line_to_cursor)
+        -- Strip the partial trigger word being typed to check the preceding context
+        local context = line_to_cursor:match("^(.-)%w*$")
+        return context ~= nil and extract_builder_type(context) ~= nil
+    end
+
+    local function is_builder_context(line_to_cursor)
+        return line_to_cursor:match("%u%w+%s+%w+%s*=%s*build$") ~= nil
+    end
+
     return {
         s("nie", {
             f(snip_utils.add_imports({
                 "org.apache.commons.lang3.NotImplementedException",
             })),
             t({ "throw new NotImplementedException();" }),
+        }),
+        s({
+            trig = "build",
+            resolveExpandParams = function(_, line_to_cursor, matched_trigger, _)
+                local context = line_to_cursor:sub(1, #line_to_cursor - #matched_trigger)
+                local type_name = extract_builder_type(context)
+                if not type_name then
+                    return nil
+                end
+                return {
+                    env_override = {
+                        BUILDER_TYPE = { type_name },
+                    },
+                }
+            end,
+        }, {
+            f(function(_, snip)
+                local type_name = snip.env.BUILDER_TYPE and snip.env.BUILDER_TYPE[1]
+                if not type_name then
+                    local line = vim.api.nvim_get_current_line()
+                    local col = vim.api.nvim_win_get_cursor(0)[2]
+                    type_name = extract_builder_type(line:sub(1, col))
+                end
+                return type_name and (type_name .. ".builder().") or ""
+            end),
+            i(1),
+            t(".build();"),
+        }, {
+            condition = is_builder_context,
+            show_condition = show_builder_context,
         }),
     }
 end
