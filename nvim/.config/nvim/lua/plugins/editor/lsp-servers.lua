8fd219f430bf8261df89660f287e6860002a7940
IUADA144 <serhii.dudar@raiffeisen.ua>
2026-04-29 14:55:26 +0300

    nvim changes

--- nvim/.config/nvim/lua/plugins/editor/lsp-servers.lua
+++ nvim/.config/nvim/lua/plugins/editor/lsp-servers.lua
@@ -2,7 +2,8 @@ return {
     -- ###### openapi #######
     -- openapi-language-server is started conditionally from ftplugin/yaml.lua
     {
-        "armsnyder/openapi-language-server",
+        -- "armsnyder/openapi-language-server",
+        "sergii-dudar/openapi-language-server",
         build = "go install .",
         ft = { "yaml" },
         lazy = true,
