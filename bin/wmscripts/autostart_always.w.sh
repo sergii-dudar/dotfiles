5af2cd30b90d874497bd1cc59421a6b47c653978
Serhii Dudar <shtanga.net@gmail.com>
2026-05-04 00:56:49 +0300

    suckless/dwl-someblocks added

--- bin/wmscripts/autostart_always.w.sh
+++ bin/wmscripts/autostart_always.w.sh
@@ -9,6 +9,12 @@
 wm_name="${1:-}"
 
 case "$wm_name" in
+    dwl)
+        # swaybg -o HDMI-A-1 -i ~/wallpapers/jpg/Wall147.jpg -m fill &
+        # swaybg -o HDMI-A-2 -i ~/wallpapers/jpg/Wall147.jpg -m fill &
+        # swaybg -o HDMI-A-3 -i ~/wallpapers/portrait/AzZxWk0.png -m fill &
+        swaybg -i ~/wallpapers/jpg/Wall147.jpg -m fill &
+        ;;
     sway)
         ;;
     hyprland)
