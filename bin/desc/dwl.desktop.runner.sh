5efd4dbe68435813a471c777b12964b88379d121
Serhii Dudar <shtanga.net@gmail.com>
2026-05-04 14:08:29 +0300

    slstatus

--- bin/desc/dwl.desktop.runner.sh
+++ bin/desc/dwl.desktop.runner.sh
@@ -1,4 +1,4 @@
-dwlblocks | exec dwl -s "\
+slstatus -s | exec dwl -s "\
     /home/serhii/dotfiles/bin/apply-display-settings.w.sh dwl; \
     /home/serhii/dotfiles/bin/wmscripts/autostart_once.w.sh dwl; \
     /home/serhii/dotfiles/bin/wmscripts/autostart_always.w.sh dwl"
