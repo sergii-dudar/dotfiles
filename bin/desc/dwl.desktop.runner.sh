ac1cb4a3eefdbeee31914fc2ab994257ad53aa0e
Serhii Dudar <shtanga.net@gmail.com>
2026-05-04 04:05:44 +0300

    dwlblocks-async

--- bin/desc/dwl.desktop.runner.sh
+++ bin/desc/dwl.desktop.runner.sh
@@ -1,5 +1,4 @@
-exec dwl -s "\
+dwlblocks | exec dwl -s "\
     /home/serhii/dotfiles/bin/apply-display-settings.w.sh dwl; \
     /home/serhii/dotfiles/bin/wmscripts/autostart_once.w.sh dwl; \
-    /home/serhii/dotfiles/bin/wmscripts/autostart_always.w.sh dwl; \
-    exec somebar"
+    /home/serhii/dotfiles/bin/wmscripts/autostart_always.w.sh dwl"
--- bin/desc/dwl.desktop.tester.sh
+++ bin/desc/dwl.desktop.tester.sh
@@ -1,4 +1,4 @@
 killall -q dwl
-killall -q someblocks
-killall -q somebar
+# killall -q someblocks
+# killall -q somebar
 /home/serhii/dotfiles/bin/desc/dwl.desktop.runner.sh > /dev/null 2>&1 &
