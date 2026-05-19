b4b58d75951208736a80b7bbd125a2060afd0814
IUADA144 <serhii.dudar@raiffeisen.ua>
2026-05-19 10:56:03 +0300

    hyprland: migration to lua config progress

--- hyprland/hypr/module/monitors.lua
+++ hyprland/hypr/module/monitors.lua
@@ -4,7 +4,6 @@
 
 -- I'm never using laptop screen, to speed, disable it manually as early as possible
 hl.monitor({ output = "eDP-1", disabled = true })
--- hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-3" })
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 -- hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
@@ -12,6 +11,9 @@ hl.monitor({ output = "eDP-1", disabled = true })
 hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "0x0", scale = 1 })
 hl.monitor({ output = "HDMI-A-2", mode = "preferred", position = "0x0", scale = 1 })
 hl.monitor({ output = "HDMI-A-3", mode = "preferred", position = "3840x0", scale = 1, transform = 3 })
+
+-- hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-3" })
+hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-3", default = true })
 
 -- output "HDMI-A-1" mode 3840x2160@60.000Hz pos 0 0
 -- output "HDMI-A-2" mode 3840x2160@60.000Hz pos 0 0
