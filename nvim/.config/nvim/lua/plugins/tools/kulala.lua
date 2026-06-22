eba3aadedc8e99504f906e9faee3a0f567a88271
IUADA144 <serhii.dudar@raiffeisen.ua>
2026-06-22 20:28:40 +0300

    changes

--- nvim/.config/nvim/lua/plugins/tools/kulala.lua
+++ nvim/.config/nvim/lua/plugins/tools/kulala.lua
@@ -16,15 +16,15 @@ return {
             { "<leader>Rr", function() require('kulala').replay() end, desc = "Replay the last request (http)" },
             { "<leader>r", "", desc = "+Rest", ft = {"http", "json"} },
             { "<leader>rr", function() require('kulala').run() end, desc = "Send the request (http)", ft = "http" },
+            { "<CR>", function() require('kulala').run() end, desc = "Send the request (http)", ft = "http" },
             { "<leader>rl", function() require('kulala').replay() end, desc = "Replay the last request (http)", ft = {"http", "json"} },
 
-            -- already defined as global_keymaps by default
             -- https://neovim.getkulala.net/docs/getting-started/default-keymaps
-            -- { "<leader>rc", function() require('kulala').copy() end, desc = "Copy as cURL", ft = "http" },
-            -- { "<leader>rC", function() require('kulala').from_curl() end, desc = "Paste from curl", ft = "http" },
-            -- { "<leader>re", function() require('kulala').set_selected_env() end, desc = "Set environment", ft = "http" },
-            -- { "<leader>rg", function() require('kulala').download_graphql_schema() end, desc = "Download GraphQL schema", ft = "http", },
-            -- { "<leader>ri", function() require('kulala').inspect() end, desc = "Inspect current request", ft = "http" },
+            { "<leader>rc", function() require('kulala').copy() end, desc = "Copy as cURL", ft = "http" },
+            { "<leader>rC", function() require('kulala').from_curl() end, desc = "Paste from curl", ft = "http" },
+            { "<leader>re", function() require('kulala').set_selected_env() end, desc = "Set environment", ft = "http" },
+            { "<leader>rg", function() require('kulala').download_graphql_schema() end, desc = "Download GraphQL schema", ft = "http", },
+            { "<leader>ri", function() require('kulala').inspect() end, desc = "Inspect current request", ft = "http" },
             -- { "<leader>rn", function() require('kulala').jump_next() end, desc = "Jump to next request", ft = "http" },
             -- { "<leader>rp", function() require('kulala').jump_prev() end, desc = "Jump to previous request", ft = "http" },
             -- { "<leader>rq", function() require('kulala').close() end, desc = "Close window", ft = "http" },
